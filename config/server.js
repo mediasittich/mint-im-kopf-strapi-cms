99163b4e369ad96365f477b0556f14037216f0f1
Regina Galambos <gina.galambos@gmail.com>
2020-08-09 12:42:01 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c292e39fea54112d901c561c5030450f'),
+    },
+  },
+});
